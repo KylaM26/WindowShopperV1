e7eaa68965d26a758989d0ca79254b7f71e6d537
Kyla <iheartkyla@yahoo.com>
2018-06-18 05:35:22 -0500

    Final Commit

--- WindowShopperV1/View/CurrencyTextField.swift
+++ WindowShopperV1/View/CurrencyTextField.swift
@@ -11,6 +11,24 @@ import UIKit
 @IBDesignable
 class CurrencyTextField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20;
+        let currenyLbl = UILabel(frame: CGRect(x: 10, y: (frame.size.height / 2) - size / 2, width: size, height: size));
+        
+        currenyLbl.backgroundColor = UIColor.clear;
+        currenyLbl.textAlignment = .center;
+        currenyLbl.textColor = UIColor.white;
+        currenyLbl.layer.cornerRadius = 5.0;
+        currenyLbl.clipsToBounds = true;
+        
+        let formatter = NumberFormatter(); // Making formatter
+        formatter.numberStyle = .currency; // Currency style.
+        formatter.locale = .current;  // The location, will determine what currency
+        
+        currenyLbl.text = formatter.currencySymbol;
+        addSubview(currenyLbl);
+    }
+    
     override func prepareForInterfaceBuilder() {
         SetPlaceHolderAttributes(); // So this class can be seen in storyboard
     }
@@ -25,6 +43,7 @@ class CurrencyTextField: UITextField {
         self.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.2497124233);
         self.layer.cornerRadius = 5.0;
         self.textAlignment = .center;
+        self.clipsToBounds = true;
         
         if let p = placeholder { // If there is a placeholder text.
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white]); // Attributed string gives more properties than regular string.
@@ -32,5 +51,4 @@ class CurrencyTextField: UITextField {
             self.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0);
         }
     }
-    
 }
